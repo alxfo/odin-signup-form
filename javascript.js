69001f3efe52c62f2f07b269dc176ca5f123df10
alxfo <120241709+alxfo@users.noreply.github.com>
2023-03-05 13:53:27 +0800

    Remove password match text

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const pwdvalidation = document.querySelector('.pwd-validation');
+const passwordInput1 = document.querySelector('.password');
+const passwordInput2 = document.querySelector('.conf-password');
+
+// if (passwordInput1.textContent !== passwordInput2.textContent) {
+//     pwdvalidation.textContent = 'Passwords must match'
+// } else {
+//     pwdvalidation.textContent = 'Passwords match'
+// }
